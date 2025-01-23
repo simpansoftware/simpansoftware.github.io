560ff641b2e9d06fe9a35f870362364895671876
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-01-23 23:14:31 +0100

    a

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var hasTouchScreen = false;
 
-var UA = navigator.userAgent;
-hasTouchScreen = (
-    /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-    /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-);
+if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} 
 
 if (hasTouchScreen) {
     var elements = document.querySelectorAll('.rectangle, .rectangle2');
